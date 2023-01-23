b9a1b08b2125677f81ceebb317c7a1198c1ea162
Nadav Alon <nadavalon98@gmail.com>
2023-01-23 15:21:34 +0200

    13k and 1 pushes

--- Assets/MenuCutsceneManager.cs
+++ Assets/MenuCutsceneManager.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 public class MenuCutsceneManager : MonoBehaviour
 {
     [SerializeField] private List<GameObject> openCutscenes;
-    private int curr
+    private int currOpenSceneInd = 0;
     [SerializeField] private List<GameObject> endCutscenes;
+    private int currEndSceneInd = 0;
 
     [SerializeField] private GameObject gameManagerObj;
     // Start is called before the first frame update
     void Start()
     {
-        startScreen.SetActive(true);
+        openCutscenes[currOpenSceneInd].SetActive(true);
     }
 
     // Update is called once per frame
