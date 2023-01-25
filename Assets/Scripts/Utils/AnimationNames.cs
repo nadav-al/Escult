f28fa459644c85bddadb12b86693a31a6dd10256
Nadav Alon <nadavalon98@gmail.com>
2023-01-25 16:20:54 +0200

    final animations

--- Assets/Scripts/Utils/AnimationNames.cs
+++ Assets/Scripts/Utils/AnimationNames.cs
@@ -6,8 +6,8 @@
     public const string ThrowDown = "Girl_Throw_Front";
     //TODO CHANGE!!!!!!!
     public const string Death = "Death_cat";
-    public const string Revive = "teleport in";
-    public const string CatLeavesLevel = "teleport out";
+    public const string Revive = "Revive";
+    public const string CatLeavesLevel = "Level_exit";
     public const string DeathState = "CatDeath";
     public const string ReviveState = "Revive";
     public const string CatDefaultState = "Idle Down";
